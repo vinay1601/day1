50f728c89d5df8c3da85d7c9f0b9b25ebcb4c97e
vinayak <vinayakarjun2018@gmail.com>
2024-08-19 12:17:22 +0530

    project 6

---
+++ src/Component/Mycomponent.jsx
@@ -0,0 +1,37 @@
+import React , { useEffect , useState } from 'react'
+
+const Mycomponent = () => {
+
+    const [ count , setCount ] = useState(0);
+    const [count2 , setCount2] = useState(10);
+
+    useEffect(() => {
+        console.log('comp is mounting....');
+
+        return function() {
+          console.log('unmounting......');
+
+        }
+    } , [count , count2] )
+
+    useEffect (() => {
+      console.log('count got Upadated' , count);
+      return function () {
+        console.log('returning count', count)
+      }
+    }, [count] )
+
+
+  return (
+    <div>
+      {/* <h3>My component</h3> */}
+      <p>value is {count} </p>
+      <p>value is {count2} </p>
+      <button onClick={() => setCount(count+1) } >Increment</button>
+      <button onClick={() => setCount2(count2+1) } >Increment2</button>
+
+    </div>
+  )
+}
+
+export default Mycomponent
